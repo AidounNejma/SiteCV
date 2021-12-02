2043f6188ce5ab04d080df5a8980649c97a321f6
AidounNejma <guermachenejma@gmail.com>
2021-12-03 00:08:57 +0100

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,16 @@ let back = document.querySelector(".back");
 let forward = document.querySelector(".forward");
 
 let text = document.querySelector(".text");
+let link = document.querySelector('.linkCarrousel');
+let image = document.querySelector('.imageCarrousel');
+
+back.addEventListener('click', slideBack);
+forward.addEventListener('click', slideNext);
+
+function slideBack(){
+    
+}
+
+function slideNext(){
+    
+}
